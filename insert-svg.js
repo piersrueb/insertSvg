2479cb691cb4a34eeebed2b0a85562428324b6e0
piersrueb <piersrueb@gmail.com>
2019-05-29 16:58:12 +0100

    replace method

--- insert-svg.js
+++ insert-svg.js
@@ -11,21 +11,12 @@ const insertSvg = (elem, method) => {
 			if (xhr.readyState != 4) return;
 			let svg = xhr.responseXML.documentElement;
 			svg = document.importNode(svg, true);
-
 			if(t[i].classList.contains('replace')){
 				svg.setAttribute('class', cl);
 				t[i].replaceWith(svg);
 			} else {
 				t[i].appendChild(svg);
 			}
-
-			// if(method === 'replace'){
-			// 	svg.setAttribute('class', cl);
-			// 	t[i].replaceWith(svg);
-			// } else {
-			// 	t[i].appendChild(svg);
-			// }
-
 		};
 		xhr.send();
 	}
