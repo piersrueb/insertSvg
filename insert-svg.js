68b7dd2ca74e458d2e63b8a8c48c75b47d17cb28
piersrueb <piersrueb@gmail.com>
2019-05-30 11:29:44 +0100

    init

--- insert-svg.js
+++ insert-svg.js
@@ -1,6 +1,6 @@
 //  insert svg
 
-const insertSvg = (elem, method) => {
+const insertSvg = (elem) => {
 	let t = document.querySelectorAll(elem)
 	for (let i = 0; i < t.length; i++) {
 		let cl = t[i].className;
